e4c5026e333bc7cf31effd6744030300c26f12f6
KioshiMichio <koliawartander@gmail.com>
2026-01-13 12:22:16 +0700

    Обновление конфига

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from models import db, Partner, calculate_discount
+from quart import Quart
+from models import #Тут должны быть таблицы
 
-app = Flask(__name__)
+app = Quart(__name__)
 
-app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SECRET_KEY'] = '1111'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///partners.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
