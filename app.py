18fedc645b51060d6a7aaea34cc5d96a2f4b7f2a
KioshiMichio <koliawartander@gmail.com>
2026-01-14 09:52:58 +0700

    Скачивание библиотеки ,создание асинхронного движка

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from config import Config
 app = Quart(__name__)
 app.config.from_object(Config)
 
+@app.before_serving
+async def startup():
+    await db.create_all()  # Создает таблицы при запуске (для разработки)
+    
 @app.route('/')
 async def home():
     return 'Hello, World!'
